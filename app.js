52bd6b984fef5d95d26388377e548ad40dbe4841
hyeonji choi <chyeon81@icloud.com>
2023-12-06 18:58:35 -0600

    edit msg

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => { 
-    res.send('Hello Jenkins!')
-    res.send('I'm Cade.')
+    res.send('TGIF Jenkins!')
 });
 
 const port = process.env.PORT || 5000;
